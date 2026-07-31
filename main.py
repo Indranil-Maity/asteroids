c3dfa9e4fe7fc29efcc876113bdac90c70f26d42
Indranil-Maity <indranilmaity1729@gmail.com>
2026-07-31 15:48:55 +0530

    added player and movements

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+if __name__ == "__main__":
+    main()
